53f289b594b9e4ac82c7ccf7b03612e3da875256
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-04-29 02:40:45 -0400

    first commit

--- src/com/talentyco/capitulo01/fundamentos/Ejercicio25.java
+++ src/com/talentyco/capitulo01/fundamentos/Ejercicio25.java
@@ -4,8 +4,6 @@ package com.talentyco.capitulo01.fundamentos;
 
     Ejercicio:
 
-
-
     @author Emmanuel Nieto Muñoz
     @version 17/12/2021/1.0
     @book Aprende Java con ejercicios, 2019, JL Sánchez González
